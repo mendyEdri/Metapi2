e7bbe6af9ddf103828cd387abdbad197e12fd4b9
Mendy Edri <mendye@wix.com>
2025-08-23 14:08:57 +0300

    Remove t-SNE visualization

--- test_clustering.py
+++ test_clustering.py
@@ -4,18 +4,10 @@ from matplotlib.figure import Figure
 from clustering import visualize_clusters
 
 
-def test_visualize_clusters_uses_pca_for_small_datasets():
-    """visualize_clusters should fall back to PCA when samples are ≤3."""
-    embeddings = np.array([[0.0, 0.0], [1.0, 1.0]])
-    labels = [0, 1]
-    fig = visualize_clusters(embeddings, labels)
-    assert isinstance(fig, Figure)
-    assert "PCA" in fig.axes[0].get_title()
-
-
-def test_visualize_clusters_clamps_perplexity():
-    """t-SNE perplexity is clamped to ``n_samples - 1`` when too large."""
+def test_visualize_clusters_uses_pca():
+    """visualize_clusters should always use PCA for dimensionality reduction."""
     embeddings = np.array([[float(i), float(i)] for i in range(5)])
     labels = [0, 1, 0, 1, 0]
     fig = visualize_clusters(embeddings, labels)
-    assert "perp=4.0" in fig.axes[0].get_title()
+    assert isinstance(fig, Figure)
+    assert "PCA" in fig.axes[0].get_title()
